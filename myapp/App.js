ab587aaad24c41f5725bc5183e942690d7c3abf9
Elly <blackcoda56@gmail.com>
2023-11-04 08:19:37 +0300

    cmt-01

--- myapp/App.js
+++ myapp/App.js
@@ -1,12 +1,37 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, SafeAreaView, Image } from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={styles.container}>
+      <Header title="Welcome to React Native"/>
+    <Text style={heading}>Step One</Text>
+    <Text>
+      Edit App.js to change this screen and turn it
+      into your app.
+    </Text>
+    <Text style={heading}>See Your Changes</Text>
+    <Text>
+      Press Cmd + R inside the simulator to reload
+      your app’s code.
+    </Text>
+    <Text style={heading}>Debug</Text>
+    <Text>
+      Press Cmd + M or Shake your device to open the
+      React Native Debug Menu.
+    </Text>
+    <Text style={heading}>Learn</Text>
+    <Text>
+      Read the docs to discover what to do next:
+    </Text>
+      {/* <Text>Hello World</Text>
+      <Image source={{
+        width: 200,
+        height:300,
+        uri:"https://picsum.photos/200/300",}} />
+      <Image source={require("./assets/splash.png")}/> */}
+      {/* <StatusBar style="auto" /> */}
+    </SafeAreaView>
   );
 }
 
