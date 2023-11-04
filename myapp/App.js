55c10621bd24604d5f0823ca4bc9f0a33e3b4de6
Elly <blackcoda56@gmail.com>
2023-11-04 10:29:30 +0300

    cmt-02

--- myapp/App.js
+++ myapp/App.js
@@ -4,32 +4,12 @@ import { StyleSheet, Text, View, SafeAreaView, Image } from 'react-native';
 export default function App() {
   return (
     <SafeAreaView style={styles.container}>
-      <Header title="Welcome to React Native"/>
-    <Text style={heading}>Step One</Text>
-    <Text>
-      Edit App.js to change this screen and turn it
-      into your app.
-    </Text>
-    <Text style={heading}>See Your Changes</Text>
-    <Text>
-      Press Cmd + R inside the simulator to reload
-      your app’s code.
-    </Text>
-    <Text style={heading}>Debug</Text>
-    <Text>
-      Press Cmd + M or Shake your device to open the
-      React Native Debug Menu.
-    </Text>
-    <Text style={heading}>Learn</Text>
-    <Text>
-      Read the docs to discover what to do next:
-    </Text>
-      {/* <Text>Hello World</Text>
+      {/* <Text>Hello World</Text> */}
       <Image source={{
         width: 200,
-        height:300,
+        height:300,      
         uri:"https://picsum.photos/200/300",}} />
-      <Image source={require("./assets/splash.png")}/> */}
+      {/* <Image source={require("./assets/splash.png")}/> */}
       {/* <StatusBar style="auto" /> */}
     </SafeAreaView>
   );
@@ -38,7 +18,7 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: 'dodgerblue',
     alignItems: 'center',
     justifyContent: 'center',
   },
