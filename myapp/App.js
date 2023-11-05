69294bf86ba1f16406a6df9e2b24830110d7b6bc
Elly <blackcoda56@gmail.com>
2023-11-05 13:59:33 +0300

    touchables

--- myapp/App.js
+++ myapp/App.js
@@ -1,14 +1,34 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, SafeAreaView, Image } from 'react-native';
+import { StyleSheet, Text, TouchableWithoutFeedback, TouchableNativeFeedback, TouchableHighlight, TouchableOpacity, View, SafeAreaView, Image } from 'react-native';
 
 export default function App() {
   return (
     <SafeAreaView style={styles.container}>
-      {/* <Text>Hello World</Text> */}
-      <Image fadeDuration={1000} borderRadius={4} source={{        
-        width: 200,
-        height:300,      
+      {/* <Text>TouchableWithoutFeedback</Text>      
+       <TouchableWithoutFeedback>
+       <Image fadeDuration={1000} borderRadius={20} source={{        
+        width: 100,
+        height:100,      
         uri:"https://picsum.photos/200/300",}} />
+       </TouchableWithoutFeedback> */}
+       
+       {/* <Text>TouchableNativeFeedback</Text>
+      <TouchableNativeFeedback>        
+      <Image fadeDuration={1000} borderRadius={20} source={{        
+      width: 100,
+      height:100,      
+      uri:"https://picsum.photos/200/300",}} />
+      </TouchableNativeFeedback>
+        */}
+      <Text>TouchableOpacity</Text>
+      <TouchableOpacity>        
+      <Image fadeDuration={2000} borderRadius={20} source={{        
+      width: 200,
+      height:200,      
+      uri:"https://picsum.photos/200/300",}} />
+      </TouchableOpacity>
+      
+      
       {/* <Image source={require("./assets/splash.png")}/> */}
       {/* <StatusBar style="auto" /> */}
     </SafeAreaView>
